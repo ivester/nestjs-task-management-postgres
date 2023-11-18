ef044db3a3db4f7f7491764b8d89e9ec90fa5f72
Ives Ebneter <ives.ebneter@gmail.com>
2023-11-18 18:45:26 +0100

    feat(tasks) add task validation pipe

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v4 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -31,7 +31,13 @@ export class TasksService {
   }
 
   getTask(id: string): Task {
-    return this.tasks.find((task) => task.id === id);
+    const task = this.tasks.find((task) => task.id === id);
+
+    if (!task) {
+      throw new NotFoundException();
+    }
+
+    return task;
   }
 
   createTask(createTaskDto: CreateTaskDto): Task {
@@ -49,7 +55,8 @@ export class TasksService {
   }
 
   deleteTask(id: string): void {
-    this.tasks = this.tasks.filter((task) => task.id !== id);
+    const task = this.getTask(id);
+    this.tasks = this.tasks.filter((t) => t.id !== task.id);
   }
 
   updateTaskStatus(id: string, status: TaskStatus): Task {
