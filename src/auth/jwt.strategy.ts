4e06581f9490ea998e8a667a1c3f5c38406ebe02
Ives Ebneter <ives.ebneter@gmail.com>
2023-11-22 20:03:01 +0100

    feat(auth): JWT validation - we can use this code to add guards later

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,31 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { UsersRepository } from './users.repository';
+import { JwtPayload } from './jwt-payload.interface';
+import { User } from './user.entity';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private usersRepository: UsersRepository) {
+    super({
+      secretOrKey: 'topSecret51',
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    });
+  }
+
+  async validate(payload: JwtPayload): Promise<User> {
+    // validate() is only called if the token is valid
+    const { username } = payload;
+    const user: User = await this.usersRepository.findOne({
+      where: { username },
+    });
+
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+
+    // user will be included in the request object and can be used in route handlers for example
+    return user;
+  }
+}
